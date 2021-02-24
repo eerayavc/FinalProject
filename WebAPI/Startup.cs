35ce5619a8457c5162a0672813211329a1bcf199
ERAY AVCI <erayavcioffical@gmail.com>
2021-02-24 21:06:04 +0300

    Güncellemeler yapıldı.Autofac eklendi.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -29,9 +29,11 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Autofac,Ninject, CastleWindsor, StructureMap, LingInject, DryInject ---> IoC Container
+            //AOP
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();//Ýçerisinde data tutmuyorsak singleton kulllanýyoruz.
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();//Ýçerisinde data tutmuyorsak singleton kulllanýyoruz.
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
